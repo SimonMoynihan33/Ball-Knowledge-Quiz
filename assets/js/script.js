582845a7f53fa004313d2aa35d2ce0a6d1615b95
Simon Moynihan <simon.moynihan33@gmail.com>
2024-07-09 14:07:33 +0000

    Change window.location.assign to .replace

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,7 +145,7 @@ function isQuizOver() {
  */
 function endQuiz() {
     localStorage.setItem('mostRecentScore', score);
-    window.location.assign('end.html');
+    window.location.replace('../../end.html');
 }
 
 /**
